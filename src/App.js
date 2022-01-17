5adc52bf6363e526018486c7af7771b4b473d316
Kécia Santos <keciakaroline@gmail.com>
2022-01-17 19:21:26 +0100

    Create new repository for dictionary project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"></header> */}
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://portfolio-keciasantos.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kécia Santos
+        </a>{" "}
+        and is{" "}
+        <a href="" target="_blank" rel="noopener noreferrer">
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://www.netlify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify.
+        </a>
+      </footer>
+    </div>
+  );
+}
