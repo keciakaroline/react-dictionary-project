2c94d377f0180fb3a7aae37c0bee0f82a4d2c45f
Kécia Santos <keciakaroline@gmail.com>
2022-01-25 19:05:16 +0100

    Added the phonetic component for the words

---
+++ src/components/Phonetic.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        {" "}
+        Click here to listen{" "}
+      </a>
+
+      {props.phonetic.text}
+    </div>
+  );
+}
